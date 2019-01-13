1d9b0feeb302a9c62e949ee357bec5283b0a9f44
Andrey Menangen <menangen@gmail.com>
2019-01-13 20:20:10 +0300

    Volume manage

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import signal
 import sys
 import docker
 
-global container
+global server_container
+VOLUME = "manager"
 
 if __name__ == '__main__':
     print("Start at: %s" % time.ctime())
@@ -17,23 +18,64 @@ if __name__ == '__main__':
         client = docker.from_env()
 
     try:
-        container = client.containers.run(
-            "menangen/travel-django-app",
-            name="web",
-            ports={"8000/tcp": 8000},
+        client.volumes.create(name=VOLUME)
+        print(f"Created <{VOLUME}> shared volume")
+    except Exception as e:
+        print(f"Can't create <{VOLUME}> volume", e)
+
+    try:
+        server_container = client.containers.run(
+            "menangen/asyncio",
+            name="server",
+            # ports={"8000/tcp": 8000},
+            volumes=[f"{VOLUME}:/tmp"],
+            tty=True,
             auto_remove=True,
             detach=True)
+        server_container.getLog = lambda: server_container.logs().decode("UTF-8")
 
-        time.sleep(3)
-        print(container.logs().decode("UTF-8"))
+        print("Is created", server_container)
+
+        client_container = client.containers.run(
+            "menangen/asyncio",
+            command="python client.py",
+            name="client",
+            # ports={"8000/tcp": 8000},
+            volumes=[f"{VOLUME}:/tmp"],
+            tty=True,
+            auto_remove=True,
+            detach=True)
+        client_container.getLog = lambda: client_container.logs().decode("UTF-8")
+
+        print("Is created", client_container)
+
+        time.sleep(1)
+        print("Server Logs: ", server_container.getLog())
+        print("Client Logs: ", client_container.getLog())
+
+        time.sleep(5)
+        print("Logs: ", server_container.getLog())
+        print("Client Logs: ", client_container.getLog())
+
+        print("waiting CTRL-C or SIGTERM...")
+
     except Exception as e:
         print("Can't start container with app", e)
 
     def end(signum, frame):
         try:
-            print("Try to remove web container")
-            container.stop()
-            print("Child container is Removed")
+            print("---------------------------------------")
+            print("Try to remove containers")
+            server_container.stop()
+            print("Server container stoped")
+            client_container.stop()
+            print("Client container stoped")
+
+            print(f"Removing unused volume: {VOLUME}")
+            time.sleep(1)
+
+            removed = client.volumes.prune()
+            print("Removed Volumes:", removed)
 
         except Exception as e:
             print("Can't remove container", e)
